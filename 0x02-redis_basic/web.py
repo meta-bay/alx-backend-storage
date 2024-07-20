2be00533aad4043d2fbdbbf665d881a4cd223a34
meta-bay <bayiemeseret@gmail.com>
2024-07-20 05:47:23 +0300

    redis basics

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for caching HTTP requests.
+Caching request module
 """
 import redis
 import requests
@@ -8,29 +8,29 @@ from functools import wraps
 from typing import Callable
 
 
-def cache_requests(fn: Callable) -> Callable:
-    """ Decorator to cache the response of a URL and track call count.
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
     """
     @wraps(fn)
     def wrapper(url: str) -> str:
         """ Wrapper that:
-            - Checks if the URL's response is cached.
-            - Tracks the number of times the URL is requested.
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
         """
-        cache_client = redis.Redis()
-        cache_client.incr(f'count:{url}')
-        cached_response = cache_client.get(f'{url}')
-        if cached_response:
-            return cached_response.decode('utf-8')
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
         response = fn(url)
-        cache_client.set(f'{url}', response, ex=10)
+        client.set(f'{url}', response, 10)
         return response
     return wrapper
 
 
-@cache_requests
+@track_get_page
 def get_page(url: str) -> str:
-    """ Makes an HTTP GET request to the specified URL.
+    """ Makes a http request to a given endpoint
     """
     response = requests.get(url)
     return response.text
